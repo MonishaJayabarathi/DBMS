2ce0d2e78ed26d944089a8218b0e19171b291952
Monisha J <mn504849@dal.ca>
2021-08-03 13:07:12 +0530

    Dump creation and Table Lock handling

--- src/main/java/Project/DataBase.java
+++ src/main/java/Project/DataBase.java
@@ -2,15 +2,19 @@ package Project;
 
 import static Project.Constants.LOCAL_PATH;
 import java.io.File;
+import java.io.IOException;
 
 public class DataBase {
 
   public String currentDatabase;
-  public void create(String name){
+  public void create(String name) throws IOException {
     File databaseFolder = new File(LOCAL_PATH+name);
     if(!databaseFolder.exists()){
       databaseFolder.mkdirs();
     }
+    File databseLock = new File(LOCAL_PATH + name + "/lock.txt");
+    databseLock.createNewFile();
+
   }
 
   public void use(String name){
